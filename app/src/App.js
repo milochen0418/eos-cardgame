ef9bb5a88e5ef79e9292de44d5259abf8fa4a2d0
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-12 19:23:38 +0000

    update App.js to support Login & Game page

--- app/src/App.js
+++ app/src/App.js
@@ -31,20 +31,32 @@ export default App;
 */
 // React core
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 // Components
 //import { Login } from './';
 import Login from './components/Login';
-
+import Game from './components/Game/Game';
 class App extends Component {
 
   render() {
+    // Extract data from state and props ('user' is from redux)
+    const { user: { name } } = this.props;
+
+    // If the username is set in redux, display the Game component
+    // If the username is NOT set in redux, display the Login component
+
     return (
       <div className="App">
-        <Login />      
+        { name && <Game /> }
+        { !name && <Login /> }
       </div>
     );
   }
 }
 
-export default App;
+//export default App;
+
+// Map all state to component props (for redux to connect)
+const mapStateToProps = state => state;
+// Export a redux connected component
+export default connect(mapStateToProps)(App);
