854f555b2d3dae0fecf811953448d326b17e5441
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-12 14:52:55 +0000

    To send the transaction, we invoke api.transact()

--- app/src/services/ApiService.js
+++ app/src/services/ApiService.js
@@ -7,6 +7,49 @@ async function takeAction(action, dataValue) {
   const rpc = new JsonRpc(process.env.REACT_APP_EOS_HTTP_ENDPOINT);
   const signatureProvider = new JsSignatureProvider([privateKey]);
   const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+  /*
+  JsonRpc - The rpc client which connects to the HTTP endpoint of an EOSIO node.
+  JsSignatureProvider - Required for signing transactions, which we create using the user’s private key. (__Using the JsSignatureProvider in the browser is not secure and should only be used for development purposes. Use a secure vault outside of the context of the webpage to ensure security when signing transactions in production)
+  Api - The client we use to communicate with an EOS blockchain node and we initialize it using the Rpc and SignatureProvider we have created
+  To use the general function takeAction() we will pass in
+  
+  action - the name of the Smart Contract action we want to invoke
+  dataValue - parameter of the Smart Contract action
+  */
+
+
+
+  /*
+  To send the transaction, we invoke api.transact() and pass it a JSON parameter. 
+  Let’s look at some attributes of interest inside the JSON:
+  account - Equivalent to the contract name
+  name - Name of the action we want to invoke
+  authorization - For signing the transaction
+  */
+ 
+  // Main call to blockchain after setting action, account_name and data
+  try {
+    const resultWithConfig = await api.transact({
+      actions: [{
+        account: process.env.REACT_APP_EOS_CONTRACT_NAME,
+        name: action,
+        authorization: [{
+          actor: localStorage.getItem("cardgame_account"),
+          permission: 'active',
+        }],
+        data: dataValue,
+      }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 30,
+    });
+    return resultWithConfig;
+  } catch (err) {
+    throw(err)
+  }
+
+
+
 }
 class ApiService {
 }
