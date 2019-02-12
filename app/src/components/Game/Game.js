09226342e38148dd29b724bdebbc2ad1a4656484
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-12 19:18:00 +0000

    add Game.js

---
+++ app/src/components/Game/Game.js
@@ -0,0 +1,15 @@
+// React core
+import React, { Component } from 'react';
+
+class Game extends Component {
+
+  render() {
+    return (
+      <section className="Game">
+        Welcome!
+      </section>
+    )
+  }
+}
+
+export default Game;
